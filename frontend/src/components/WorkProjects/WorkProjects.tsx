89c9d1cb595eb0092494fe3bb7a796d992bbddeb
NoelVar <varnoel11@gmail.com>
2025-07-28 17:20:54 +0100

    Added loading to projects

--- frontend/src/components/WorkProjects/WorkProjects.tsx
+++ frontend/src/components/WorkProjects/WorkProjects.tsx
@@ -3,12 +3,25 @@ import '../../css/util.css';
 import { useEffect, useState } from 'react';
 import ProjectCard from './ProjectCards/ProjectCard';
 import { Project } from '../../models/project';
+import { Bounce, toast } from 'react-toastify';
 
 const WorkProjects = () => {
     const [projects, setProjects] = useState<Project[]>([]);
 
     useEffect(() => {
         async function loadProjects() {
+            const id = toast.loading("Loading projects...", 
+            {
+                position: "bottom-right",
+                autoClose: 5000,
+                hideProgressBar: false,
+                closeOnClick: false,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+                theme: "dark",
+                transition: Bounce,
+            });
             try {
                 const response = await fetch('https://portfolio-typescript.up.railway.app/api/projects/', 
                     {
@@ -16,8 +29,41 @@ const WorkProjects = () => {
                     }
                 );
 
+                if (!response.ok) {
+                    toast.update(id, 
+                    {   
+                        render: "Couldn't load projects", 
+                        type: "error", 
+                        isLoading: false, 
+                        position: "bottom-right",
+                        autoClose: 5000,
+                        hideProgressBar: false,
+                        closeOnClick: true,
+                        pauseOnHover: true,
+                        draggable: true,
+                        progress: undefined,
+                        theme: "dark",
+                        transition: Bounce,
+                    });
+                }
+
                 const projects = await response.json();
                 setProjects(projects);
+                toast.update(id, 
+                {   
+                    render: "Projects loaded successfully!", 
+                    type: "success", 
+                    isLoading: false, 
+                    position: "bottom-right",
+                    autoClose: 5000,
+                    hideProgressBar: false,
+                    closeOnClick: true,
+                    pauseOnHover: true,
+                    draggable: true,
+                    progress: undefined,
+                    theme: "dark",
+                    transition: Bounce,
+                });
             } catch (error) {
                 console.error(error);
             }
