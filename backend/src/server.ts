46c1c44d7300444b0ffe7a30f97b527f03e71488
NoelVar <varnoel11@gmail.com>
2025-07-28 17:44:57 +0100

    Added back Mongo and server connection

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -6,14 +6,14 @@ import env from './util/validateEnv';
 // constants/variables
 const port = env.PORT;
 
-// Start server immediately
-const server = app.listen(port, () => {
-    console.log('Server is running on port: ' + port);
-});
-
-// Connect to MongoDB in background
+// Connect to MongoDB
 mongoose.connect(env.MONGO_CONN)
-    .then(() => console.log("Mongoose connected"))
+    .then(() => {
+        console.log("Mongoose connected")
+        app.listen(port, () => {
+            console.log('Server is running on port: ' + port);
+        });
+    })
     .catch(console.error);
 
 // End of Document --------------------------------------------------------------------------------
