bec5d09a7d242e1116afad7b73ce670e96305780
NoelVar <varnoel11@gmail.com>
2026-08-08 15:08:20 +0100

    Added responsive styling to Landing page

--- frontend-v2/src/components/Card/Card.tsx
+++ frontend-v2/src/components/Card/Card.tsx
@@ -1,12 +1,12 @@
 const Card = ({ name, image  }: {name: string, image: string}) => {
     return (
-        <div className="flex gap-3 my-5 hover:bg-[#0e3da7] hover:cursor-pointer p-2 rounded">
+        <div className="flex bg-[#0e3da7]/40 md:bg-transparent gap-3 my-5 hover:bg-[#0e3da7] hover:cursor-pointer p-2 rounded">
             <img
                 src={`../../../public/${image}`}
                 alt={`${name} image`} 
-                className="w-15 h-15 rounded border border-white shadow-lg"
+                className="w-25 h-25 md:w-20 md:h-20 lg:w-18 lg:h-18 rounded border border-white shadow-lg"
             />
-            <h2>{name}</h2>
+            <h2 className="text-[18px] md:text-[16px] text-shadow-lg">{name}</h2>
         </div>
     );
 }
--- frontend-v2/src/components/Landing/Landing.tsx
+++ frontend-v2/src/components/Landing/Landing.tsx
@@ -3,22 +3,22 @@ import Card from "../Card/Card";
 const Landing = () => {
     return ( 
         <div className="h-screen flex flex-col">
-            <div className="w-full h-20 border-b border-white/40 shrink-0" style={{ "backgroundColor": "#2331ac"}}></div>
-            <div className="flex-1 animate-appearance [animation-delay:0.5s] flex items-center justify-center gap-5 text-white w-full">
-                <div>
+            <div className="w-full md:h-20 border-b-2 border-white/60 shrink-0" style={{ "backgroundColor": "#2331ac"}}></div>
+            <div className="flex-1 animate-appearance [animation-delay:0.5s] flex flex-col md:flex-row items-center justify-center gap-10 md:gap-5 text-white w-full">
+                <div className="flex flex-col items-center gap-2 md:items-end justify-center">
                     <img 
-                        src="../../../public/temp_log.png"
+                        src="/temp_logo2.png"
                         alt="logo"
-                        className="w-35"
+                        className="h-35"
                     />
-                    <p>To begin, click your matching profile</p>
+                    <p className="text-[20px] md:text-[16px] text-shadow-lg">To begin, click your matching profile</p>
                 </div>
-                <div className="border-l border-white/40 px-5 shrink-0">
+                <div className="md:border-l border-white/40 px-5 shrink-0 w-full md:w-100">
                     <Card name="Recruiter" image="IMG_5460.JPG"/>
                     <Card name="Guest" image="P1040718.JPG"/>
                 </div>
             </div>
-            <div className="w-full h-20 border-t-2 border-white/60" style={{ "backgroundColor": "#2331ac"}}></div>
+            <div className="w-full md:h-20 border-t-2 border-white/60" style={{ "backgroundColor": "#2331ac"}}></div>
         </div>
      );
 }
